1ea1cb02b9b07c2b4d0dfbb645088ad71a0d107f
IryDev <86270481+IryDev@users.noreply.github.com>
2023-06-03 18:39:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var navbarLinks = document.querySelector(".nav-links");
 
 function volet() {
   navbarLinks.classList.toggle("mobile-menu");
-
+  hamburgerMenu.classList.toggle("open");
 }
 
 hamburgerMenu.addEventListener("click", volet);
